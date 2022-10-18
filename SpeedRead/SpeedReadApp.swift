85982c5a1895acadc788472e669e01c8db35b836
DManap <DManap@beeline.kz>
2022-10-18 18:17:27 +0600

    Initial Commit

---
+++ SpeedRead/SpeedReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedReadApp.swift
+//  SpeedRead
+//
+//  Created by Dias Manap on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpeedReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
